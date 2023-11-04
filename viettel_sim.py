4185ad5c91ad1d95765aa01e31921db2e0368e22
tonysodeep <hynguyen2727@gmail.com>
2023-11-04 10:39:01 +0700

    fixed start runing bug and close the pop up window

--- viettel_sim.py
+++ viettel_sim.py
@@ -4,15 +4,14 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 
-driver = webdriver.Edge()
+driver = webdriver.Chrome()
 url= "https://vietteltelecom.vn/di-dong/sim-so"
 driver.maximize_window()
 driver.get(url)
 
-time.sleep(5)
+time.sleep(10)
 
-driver.find_element('xpath','//*[@id="app"]/div[1]/div[16]/div[2]/h2').click()
-
+driver.find_element('xpath','//*[@id="close-button-1454703513202"]').click()
 more_sim =  driver.find_element('xpath','//*[@id="sim-tt"]/div/div/a').click()
 
 time.sleep(5)
@@ -23,13 +22,16 @@ table = soup.find("table",{"class":"sim-number__table"})
 
 headers=[]
 titles = []
-df = pd.DataFrame(columns=titles)
+
 
 
 #get all header of table
 headers = table.find_all('th')
 for header in headers:
     titles.append(header.text)
+
+df = pd.DataFrame(columns=titles)
+
 rows = table.find_all('tr')
 for row in rows[1:]:
     data = row.find_all('td')
